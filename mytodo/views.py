1b6a52f2de20988a96a6b6cbdb391b816303b042
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2025-05-29 14:35:04 +0900

    add タスク一覧表示

--- mytodo/views.py
+++ mytodo/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views import  View
+from .models import Task
 
 class IndexView(View):
     def get(self, request):
-        # todoリスト取得
+        # todoリストを取得
+        todo_list = Task.objects.all()
+        context = {"todo_list": todo_list}
 
         # テンプレートをレンダリング
-        return render(request, "mytodo/index.html")
+        return render(request, "mytodo/index.html", context)
 
 # ビュークラスをインスタンス化
 index = IndexView.as_view()
